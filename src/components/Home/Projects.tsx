3c9def3ee45bff8fbefa8b64fbf0482799a5986b
Gustavo Keppler <keppler.gustavo@gmail.com>
2023-08-17 09:56:22 -0300

    folder filling projects.tsx

---
+++ src/components/Home/Projects.tsx
@@ -0,0 +1,11 @@
+interface ProjectsProp{
+    projects: any;
+}
+
+export const Projects = ({projects}: ProjectsProp) => {
+    return(
+        <div>
+            <h1>Components/Home/Project</h1>
+        </div>
+    );
+};
