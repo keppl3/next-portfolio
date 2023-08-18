3bb246d012ae270a792c9c76206e09590d8e0354
Gustavo Keppler <keppler.gustavo@gmail.com>
2023-08-18 08:13:50 -0300

    create page projects

--- src/components/Home/Projects.tsx
+++ src/components/Home/Projects.tsx
@@ -1,11 +1,19 @@
+import { Project } from "@/types/home";
+
 interface ProjectsProp{
-    projects: any;
+    projects: Project[];
 }
 
 export const Projects = ({projects}: ProjectsProp) => {
     return(
-        <div>
-            <h1>Components/Home/Project</h1>
-        </div>
+        <article>
+            <h2>projetos recentes</h2>
+
+            <ul>
+                {projects.map(({slug,name,image})=>   (
+                    
+                ))}              
+            </ul>
+        </article>
     );
 };
