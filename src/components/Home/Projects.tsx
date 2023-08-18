ce4cb25e631e1a0f1ff4d54ac9b915d1c3269cfc
Gustavo Keppler <keppler.gustavo@gmail.com>
2023-08-18 10:47:38 -0300

    inclui imagens dos projetos

--- src/components/Home/Projects.tsx
+++ src/components/Home/Projects.tsx
@@ -1,4 +1,6 @@
 import { Project } from "@/types/home";
+import Image from "next/image";
+import Link from "next/link";
 
 interface ProjectsProp{
     projects: Project[];
@@ -10,8 +12,18 @@ export const Projects = ({projects}: ProjectsProp) => {
             <h2>projetos recentes</h2>
 
             <ul>
-                {projects.map(({slug,name,image})=>   (
-                    
+                {projects.map(({slug,name,image}, index) =>   (
+                   <link href="{'/projects/${slug}'} key={name + index}">
+                       <li>
+                          <Image
+                            src={image.url}
+                            alt={image.alt}
+                            width={300}
+                            height={300}
+                            className=""
+                          />
+                       </li>
+                   </link>
                 ))}              
             </ul>
         </div>
